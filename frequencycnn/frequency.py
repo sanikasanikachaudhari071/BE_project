6313053461bf0ddd28e9fbf720ee9569d3aefc0f
sanikasanikachaudhari071 <sanikachaudhari071@gmail.com>
2026-03-31 00:03:54 +0530

    hope

--- frequencycnn/frequency.py
+++ frequencycnn/frequency.py
@@ -101,7 +101,7 @@ def train_one_epoch(model, loader, optimizer, device):
 def get_frequency_vectors(freq_np, labels_np, device, epochs=5):
 
     dataset = FrequencyDataset(freq_np, labels_np)
-    loader = DataLoader(dataset, batch_size=16, shuffle=True)
+    loader = DataLoader(dataset, batch_size=16, shuffle=False)
 
     model = FrequencyCNN().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
